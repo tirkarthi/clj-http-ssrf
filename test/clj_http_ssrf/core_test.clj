e4b93ec97b5bd0c1eff737bdf21a1c371067131a
Ackerley Tng <ackerleytng@gmail.com>
2018-03-03 16:57:23 +0800

    Improve test cases to include positive test cases

--- test/clj_http_ssrf/core_test.clj
+++ test/clj_http_ssrf/core_test.clj
@@ -1,31 +1,69 @@
 (ns clj-http-ssrf.core-test
   (:require [clojure.test :refer :all]
             [clj-http.client :as client]
-            [clj-http-ssrf.core :refer :all]))
+            [clj-http-ssrf.core :refer :all]
+            [clj-http-ssrf.reserved :as r]))
+
+(def successful-results
+  {:status 999 :headers {"Successful" "true"} :body "Success"})
+
+(def default-results
+  {:status 403, :headers {}, :body ""})
+
+(defn success-middleware
+  "Middleware to stop actual requests from being made"
+  [url & options]
+  (fn [client] successful-results))
+
+(defn get-with-middleware
+  [middleware url]
+  (client/with-middleware
+    ;; Use success-middleware for testing purposes
+    [success-middleware middleware]
+    (client/get url)))
 
 (deftest regex-test
   (testing "Test against regex URLs"
-    (is (= (client/with-middleware (conj client/default-middleware (wrap-validators :regexes [#"localhost"])) (client/get "http://localhost/api/private/"))
-           {:status 403, :headers {}, :body ""}))
-    (is (= (client/with-middleware (conj client/default-middleware (wrap-validators :regexes [#"private"])) (client/get "http://localhost/api/private/"))
-           {:status 403, :headers {}, :body ""}))))
+    (is (= default-results
+           (get-with-middleware (wrap-validators :regexes [#"localhost"])
+                                "http://localhost/api/private/")))
+    (is (= default-results
+           (get-with-middleware (wrap-validators :regexes [#"private"])
+                                "http://localhost/api/private/")))
+    (is (= successful-results
+           (get-with-middleware (wrap-validators :regexes [#"database"])
+                                "http://www.google.com")))))
 
 (deftest port-test
   (testing "Port test"
-    (is (= (client/with-middleware (conj client/default-middleware (wrap-validators :ports [6379 5672])) (client/get "http://localhost:6379"))
-           {:status 403, :headers {}, :body ""}))))
+    (is (= default-results
+           (get-with-middleware (wrap-validators :ports [6379 5672])
+                                "http://localhost:6379")))
+    (is (= successful-results
+           (get-with-middleware (wrap-validators :ports [1111])
+                                "http://localhost:6379")))))
 
 (deftest ip-test
   (testing "IP test"
-    (is (= (client/with-middleware (conj client/default-middleware (wrap-validators :hosts ["127.0.0.1"])) (client/get "http://127.0.0.1"))
-           {:status 403, :headers {}, :body ""}))))
+    (is (= default-results
+           (get-with-middleware (wrap-validators :hosts ["127.0.0.1"])
+                                "http://127.0.0.1")))
+    (is (= successful-results
+           (get-with-middleware (wrap-validators :hosts ["127.0.0.1"])
+                                "http://8.8.8.8")))))
 
 (deftest subnet-test
   (testing "Subnet test"
-    (is (= (client/with-middleware (conj client/default-middleware (wrap-validators :hosts ["10.0.0.0/1"])) (client/get "http://10.0.10.11/secret/private/endpoint/"))
-           {:status 403, :headers {}, :body ""}))))
+    (is (= default-results
+           (get-with-middleware (wrap-validators :hosts ["10.0.0.0/1"])
+                                "http://10.0.10.11/secret/private/endpoint/")))
+    (is (= successful-results
+           (get-with-middleware (wrap-validators
+                                 :hosts [(get-in r/reserved-ip-data [:private :local-a])])
+                                "http://192.168.1.1/index.html")))))
 
 (deftest custom-status-code-test
   (testing "Test with custom status code"
-    (is (= (client/with-middleware (conj client/default-middleware (wrap-validators :status 404 :regexes [#"google"])) (client/get "http://google.com"))
-           {:status 404, :headers {}, :body ""}))))
+    (is (= {:status 404, :headers {}, :body ""}
+           (get-with-middleware (wrap-validators :status 404 :regexes [#"google"])
+                                "http://google.com")))))
