7a2187e1fd1a930be21081084eab4613d43fc765
xtreak <tir.karthi@gmail.com>
2018-04-19 16:12:01 +0530

    Initial commit of docs with codox

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-http "3.7.0"]
-                 [com.damballa/inet.data "0.5.7"]])
+                 [com.damballa/inet.data "0.5.7"]]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox {:output-path "docs"
+          :source-uri "http://github.com/tirkarthi/clj-http-ssrf/blob/{version}/{filepath}#L{line}"})
